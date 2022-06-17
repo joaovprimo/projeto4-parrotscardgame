4505d7712541cfb29fe6cc430e8338467802809a
joaovprimo <fulano@email.com>
2022-06-17 10:49:42 -0300

    atualizado01 - 17/06 - aplicada a distribuicao de cartas

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ let arrImg=[
 `<img src="./revertitparrot.gif">`,
 `<img src="./tripletsparrot.gif">`,
 `<img src="./unicornparrot.gif">`];
-
+let card;
 
 function cardGame(){
-    let card = prompt("Insira a quantidade de cartas que irá jogar:");
+    card = prompt("Insira a quantidade de cartas que irá jogar:");
     let carta= card;
 while(carta<4){
     carta = prompt("Numero inválido, insira um número par entre 4 e 14:");
@@ -21,5 +21,20 @@ while(carta>14){
 while(carta%2==!0){
     carta = prompt("Numero inválido, insira um número par entre 4 e 14:");
 }console.log(carta);
+distribuiCartas(carta);
 }
 
+function distribuiCartas(elemento){
+    document.querySelector(".cards").innerHTML="";
+
+for(let cont=0; cont<elemento;cont++){
+    let adcCards= `<div class="card"> 
+    <div class="card-back">
+        <img class="img-back" src="./front.png">
+    </div>
+    <div class="card-front">
+        <img class="img-front" src="./metalparrot.gif">
+    </div>`;
+    document.querySelector(".cards").innerHTML += adcCards;
+}
+}
