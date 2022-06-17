71196432c3b99fe515ad90644b3e0f3b7c5b8bf4
joaovprimo <fulano@email.com>
2022-06-17 17:51:02 -0300

    Atualizado03 - 17/06 - cartas virando e desvirando

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 let arrImg=[
 `<img class="img-front" src="./bobrossparrot.gif">`,
 `<img class="img-front" src="./explodyparrot.gif">`,
@@ -10,10 +11,13 @@ let arrImg=[
 ];
 let contador = arrImg.length;
 let card;
-
+cardGame()
 function cardGame(){
-    card = prompt("Insira a quantidade de cartas que irá jogar:");
+    card = Number(prompt("Insira a quantidade de cartas que irá jogar:"));
     let carta= card;
+while(isNaN(carta)){
+        carta = prompt("Numero inválido, insira um número par entre 4 e 14:");
+    }
 while(carta<4){
     carta = prompt("Numero inválido, insira um número par entre 4 e 14:");
 }console.log(carta);
@@ -24,8 +28,8 @@ while(carta%2==!0){
     carta = prompt("Numero inválido, insira um número par entre 4 e 14:");
 }console.log(carta);
 distribuiCartas(carta);
-}
 
+}
 function comparador(){
     return Math.random() - 0.5; 
 }
@@ -43,14 +47,32 @@ for(j=arrImg.length;j>pares;j--){
 for(i=0; i<2;i++){
 arrImg.sort(comparador);
 for(let cont=0; cont<pares;cont++){
-    let adcCards= `<div class="card"> 
+    let adcCards= `<div class="card inicial" onclick="selecionaCarta(this)"> 
     <div class="card-back">
         <img class="img-back" src="./front.png">
     </div>
-    <div class="card-front">
+    <div class="card-front desativa">
         ${arrImg[cont]}
     </div>`;
     cards.innerHTML+=adcCards;
 }
 }
 }
+function selecionaCarta(elemento){
+    let selecionado = elemento;
+    let inicial;
+    inicial = selecionado.classList.contains("inicial");
+if(inicial){
+    selecionado.classList.remove("inicial");
+    let pegacarta = elemento.querySelector(".card-back");
+    pegacarta.classList.remove("desativa");
+    let pegacarta2= elemento.querySelector(".card-front");
+    pegacarta2.classList.add("desativa");
+} else{
+    selecionado.classList.add("inicial");
+    let pegacarta = elemento.querySelector(".card-back");
+    pegacarta.classList.add("desativa");
+    let pegacarta2= elemento.querySelector(".card-front");
+    pegacarta2.classList.remove("desativa");
+    }
+    }
